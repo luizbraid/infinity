d9c9d847614511a1eaa5d08e00ca7f97f1ec8027
Luiz Braid <luizbraid@gmail.com>
2024-10-09 22:18:48 -0300

    Add files via upload

---
+++ aula 01 - Variaveis/app.py
@@ -0,0 +1,21 @@
+#Aula de VAriaveis
+
+#Tipos de Dados Primitivos
+nome = "Luiz Braid" #tipo texto (String)
+idade = 43 #tipo inteiro (int)
+altura = 1.75 #tipo flutuante (floot)
+temCNH = True #tipo booleano (boolean)
+
+# Imprimindo as variaveis na tela
+print ("Usuario - ",nome," \nIdade - ",idade," \nAltura - ",altura)
+
+
+
+#Imprimindo os tipos de variaveis
+print(type(nome),'\n',type(idade),'\n',type(altura),'\n',type(temCNH))
+
+
+# recebendo variavel do usuario
+nomeDigitado=input("Ola, Digite seu nome: ")
+print("O nome digitado foi - ",nomeDigitado)
+
---
+++ aula 01 - Variaveis/pratica01.py
@@ -0,0 +1,12 @@
+
+#Atividade pratica
+saudacao = "Hello World"
+print(saudacao)
+#
+seuNome = input('Digite seu nome:' )
+suaIdade = int(input('Digite sua Idade:' ))
+suaAltura = float(input('Digite sua Altura:'))
+#
+print ("Nome: ",seuNome,"\nIdade: ",suaIdade,"Anos", "\nAltura:",suaAltura,"m")
+print (type(seuNome),"\n",type(suaIdade),"\n",type(suaAltura))
+
